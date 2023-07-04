f5969453b727e885a4388facd5f07f64a414ee4c
Yen Lin <e19970722@gmail.com>
2023-07-04 13:35:53 +0800

    Initial Commit

---
+++ 20230627_BingoAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20230627_BingoAPP
+//
+//  Created by Yen Lin on 2023/7/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
